5dea92ec220f472c763b21d9858a4f9964218028
Eli Nicksic <elinicksic@gmail.com>
2022-02-01 21:39:02 -0600

    Greeting and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
+import { GoMarkGithub } from 'react-icons/go';
+import { ImMail4 } from 'react-icons/im'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <h1>Hello</h1>
+      <div className={styles.bighello}>
+        <Image src="/pfp.png" alt="My Profile Picture" width="200" height="200" className={styles.profilepicture} />
+        <div className={styles.bighelloright}>
+          <h1 className={styles.bighellotitle}>Hello, I&apos;m<br />Eli Nicksic</h1>
+          <div className={styles.icons}>
+            <a href="https://github.com/Dolphinbob/">
+              <GoMarkGithub className={styles.icon} size="60" />
+            </a>
+            <a href="mailto:me@elinicksic.me">
+              <ImMail4 className={styles.icon} size="60" />
+            </a>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
