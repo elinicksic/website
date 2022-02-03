c8d5488c606800a7e3fa018fc16c70aa62d15855
Eli Nicksic <elinicksic@gmail.com>
2022-02-03 11:45:17 -0600

    Add scroll-down thing and fix responsive centering problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,32 @@ import Layout from '../components/layout'
 import styles from '../styles/Home.module.css'
 import { GoMarkGithub } from 'react-icons/go';
 import { ImMail4 } from 'react-icons/im'
+import { IoIosArrowDown } from 'react-icons/io'
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className={styles.bighello}>
-        <Image src="/pfp.png" alt="My Profile Picture" width="250" height="250" layout="intrinsic" className={styles.profilepicture} />
-        <div className={styles.bighelloright}>
-          <h1 className={styles.bighellotitle}>Hello, I&apos;m<br /><span className={styles.name}>Eli Nicksic</span></h1>
-          <div className={styles.icons}>
-            <a href="https://github.com/Dolphinbob/">
-              <GoMarkGithub className={styles.icon} size="60" />
-            </a>
-            <a href="mailto:me@elinicksic.me">
-              <ImMail4 className={styles.icon} size="60" />
-            </a>
+      <div className={styles.mainScreen}>
+        <div className={styles.bighello}>
+          <Image src="/pfp.png" alt="My Profile Picture" width="250" height="250" layout="intrinsic" className={styles.profilepicture} />
+          <div className={styles.bighelloright}>
+            <h1 className={styles.bighellotitle}>Hello, I&apos;m<br /><span className={styles.name}>Eli Nicksic</span></h1>
+            <div className={styles.icons}>
+              <a href="https://github.com/Dolphinbob/">
+                <GoMarkGithub className={styles.icon} size="60" />
+              </a>
+              <a href="mailto:me@elinicksic.me">
+                <ImMail4 className={styles.icon} size="60" />
+              </a>
+            </div>
           </div>
         </div>
+        <div className={styles.scrollDown}>
+          <p>Scroll Down!</p>
+          <IoIosArrowDown />
+        </div>
       </div>
+      <p>Hi</p>
     </Layout>
   )
 }
